5740367facf4e84ff3980fc16b036a756a01f64d
Raiyan <tajkierhaque@gmail.com>
2023-01-06 12:32:03 +0600

    [Add] sign up page UI done

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import Header from "components/Header/Header";
 import { useState } from "react";
 import { BsEyeFill, BsEyeSlash } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function Login() {
     const [togglePassword, setTogglePassword] = useState(false);
@@ -10,23 +11,26 @@ function Login() {
             <Header />
             <section>
                 <div className="mt-20 flex items-center justify-center">
-                    <form className="flex w-2/6 flex-col space-y-6 rounded-xl border bg-white p-6 shadow">
+                    <form className="flex w-2/6 flex-col space-y-4 rounded-xl border bg-white p-6 shadow">
                         <h4 className="text-center text-xl font-semibold text-indigo-500">Login</h4>
-                        <input
-                            className="rounded border-gray-400 py-3 text-sm focus:outline-none focus:ring-0"
-                            required
-                            type="email"
-                            autoComplete="off"
-                            placeholder="Email"
-                        />
-                        <label className="relative" htmlFor="password">
+                        <label className="flex flex-col space-y-2" htmlFor="email">
+                            <span className="text-xs">Email</span>
+                            <input
+                                id="email"
+                                className="rounded border-gray-400 py-3 text-sm focus:outline-none focus:ring-0"
+                                required
+                                type="email"
+                                autoComplete="off"
+                            />
+                        </label>
+                        <label className="relative flex flex-col space-y-2" htmlFor="password">
+                            <span className="text-xs">Password</span>
                             <input
                                 className="w-full rounded border-gray-400 py-3 text-sm focus:outline-none focus:ring-0"
                                 required
                                 id="password"
                                 type={`${togglePassword ? "text" : "password"}`}
                                 autoComplete="off"
-                                placeholder="Password"
                             />
                             {togglePassword ? (
                                 <button
@@ -46,6 +50,16 @@ function Login() {
                                 </button>
                             )}
                         </label>
+
+                        <div className="flex justify-between text-xs">
+                            <p>
+                                Don't have an account ?{" "}
+                                <Link className="text-indigo-600" to="/register">
+                                    Sign up
+                                </Link>
+                            </p>
+                            <p className="text-indigo-600">Forget password?</p>
+                        </div>
                         <button
                             className="rounded-lg bg-gray-800 py-3 text-sm text-white"
                             type="submit"
--- client/src/pages/register/register.tsx
+++ client/src/pages/register/register.tsx
@@ -1,5 +1,89 @@
+import Header from "components/Header/Header";
+import { useState } from "react";
+import { BsEyeFill, BsEyeSlash } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 function Register() {
-    return <div>register</div>;
+    const [togglePassword, setTogglePassword] = useState(false);
+
+    return (
+        <>
+            <Header />
+            <section>
+                <div className="my-20 flex items-center justify-center">
+                    <form className="flex w-2/6 flex-col space-y-4 rounded-xl border bg-white p-6 shadow">
+                        <h4 className="text-center text-xl font-semibold text-indigo-500">
+                            Sign up
+                        </h4>
+                        <label className="flex flex-col space-y-2" htmlFor="name">
+                            <span className="text-xs">Name</span>
+                            <input
+                                id="name"
+                                className="rounded border-gray-400 py-3 text-sm focus:outline-none focus:ring-0"
+                                required
+                                name="name"
+                                type="text"
+                                autoComplete="off"
+                            />
+                        </label>
+                        <label className="flex flex-col space-y-2" htmlFor="email">
+                            <span className="text-xs">Email</span>
+                            <input
+                                id="email"
+                                name="email"
+                                className="rounded border-gray-400 py-3 text-sm focus:outline-none focus:ring-0"
+                                required
+                                type="email"
+                                autoComplete="off"
+                            />
+                        </label>
+                        <label className="relative flex flex-col space-y-2" htmlFor="password">
+                            <span className="text-xs">Password</span>
+                            <input
+                                className="w-full rounded border-gray-400 py-3 text-sm focus:outline-none focus:ring-0"
+                                required
+                                id="password"
+                                name="password"
+                                type={`${togglePassword ? "text" : "password"}`}
+                                autoComplete="off"
+                            />
+                            {togglePassword ? (
+                                <button
+                                    onClick={() => setTogglePassword(!togglePassword)}
+                                    type="button"
+                                    className="absolute right-2 h-full text-2xl"
+                                >
+                                    <BsEyeSlash />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => setTogglePassword(!togglePassword)}
+                                    type="button"
+                                    className="absolute  right-2 h-full text-2xl"
+                                >
+                                    <BsEyeFill />
+                                </button>
+                            )}
+                        </label>
+
+                        <p className="text-center text-xs">
+                            Already have an account ?{" "}
+                            <Link className="text-indigo-600" to="/login">
+                                Login
+                            </Link>
+                        </p>
+
+                        <button
+                            className="rounded-lg bg-gray-800 py-3 text-sm text-white"
+                            type="submit"
+                        >
+                            Sign up
+                        </button>
+                    </form>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default Register;
