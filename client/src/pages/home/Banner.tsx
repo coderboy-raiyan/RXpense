5b1af3ea4416d861d6acbbb12dd49305dad62d6d
Raiyan <tajkierhaque@gmail.com>
2023-01-12 12:42:57 +0600

    [Add] Banner done

--- client/src/pages/home/Banner.tsx
+++ client/src/pages/home/Banner.tsx
@@ -1,5 +1,34 @@
+import images from "assets";
+import { BsArrowRightCircleFill } from "react-icons/bs";
+
 function Banner() {
-    return <div>Banner</div>;
+    return (
+        <section className="border-general relative -mt-6 w-full overflow-hidden overflow-x-clip border-b bg-slate-50 bg-gradient-to-t from-slate-50 to-slate-100 lg:h-screen">
+            <div
+                style={{ backgroundImage: `url(${images.bannerBg})` }}
+                className="mx-4 flex flex-col items-center justify-between py-10 md:mx-auto lg:mx-auto lg:h-full lg:max-w-6xl lg:flex-row lg:py-0"
+            >
+                {/* left side */}
+                <div className="w-full space-y-6 md:w-2/4 lg:w-2/4">
+                    <h1 className="text-4xl font-semibold text-gray-800 lg:text-5xl">
+                        The <span className="text-indigo-600 underline">Expense Tracker</span>{" "}
+                        <br /> that works for you
+                    </h1>
+                    <p className="font-bold text-gray-800">Track all your expenses here...</p>
+                    <button
+                        className="flex items-center rounded border-2 border-indigo-600 bg-indigo-600 py-2 px-4 text-white transition hover:bg-indigo-100 hover:text-indigo-600"
+                        type="button"
+                    >
+                        Get Stated <BsArrowRightCircleFill className="ml-2 text-xl" />
+                    </button>
+                </div>
+                {/* Right side */}
+                <div className="mb-4 w-full space-y-4 md:w-2/4 lg:mb-0 lg:w-2/4">
+                    <img className="w-[620px] object-contain" src={images?.bannerVector} alt="" />
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Banner;
--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,32 +1,7 @@
-import images from "assets";
+import Banner from "./Banner";
 
 function Home() {
-    return (
-        <section className="border-general relative -mt-6 w-full overflow-hidden overflow-x-clip border-b bg-slate-50 bg-gradient-to-t from-slate-50 to-slate-100 lg:h-screen">
-            <div className="absolute right-[28%] top-0 hidden h-[150px] w-[200px] rotate-12 rounded-3xl bg-gradient-to-l from-blue-600 to-sky-400 opacity-20 blur-3xl filter dark:block dark:opacity-30 lg:top-44 lg:-right-20 lg:h-72 lg:w-[350px] xl:h-80 xl:w-[500px]" />
-            <div className="absolute bottom-44 -left-64 hidden h-[150px] w-[900px] -rotate-45 rounded-3xl bg-gradient-to-r from-violet-600 to-indigo-800 opacity-30 blur-3xl filter dark:block lg:bottom-24 lg:-left-20 lg:h-28 lg:w-[250px] lg:-rotate-12 lg:opacity-20 xl:h-40 xl:w-[400px]" />
-            <div className="absolute left-[28%] top-28 hidden rotate-12 rounded-3xl bg-sky-800 opacity-90 blur-3xl filter dark:opacity-30 lg:h-32 lg:w-[450px] dark:lg:block xl:h-44 xl:w-[600px]" />
-            <div className="absolute h-16 w-[600px] rotate-[-40deg] rounded-3xl bg-sky-400 opacity-10 blur-2xl filter dark:hidden lg:bottom-24 lg:-left-28 lg:h-12 lg:w-[600px] lg:opacity-30 lg:blur-2xl xl:-left-40 xl:h-4 xl:w-[700px] xl:opacity-100" />
-            <div className="absolute h-14 w-[600px] rotate-[-40deg] rounded-3xl bg-purple-400 opacity-30 blur-2xl filter dark:hidden lg:bottom-20 lg:-left-28 lg:h-10 lg:w-[600px] lg:opacity-20 lg:blur-xl xl:-left-40 xl:h-2 xl:w-[800px] xl:opacity-100" />
-            <div className="absolute hidden h-16 w-[600px] rotate-[-40deg] rounded-3xl bg-sky-400 opacity-10 blur-2xl filter dark:hidden lg:top-24 lg:-right-28 lg:block lg:h-12 lg:w-[600px] lg:opacity-30 lg:blur-2xl xl:-right-40 xl:h-4 xl:w-[700px] xl:opacity-100" />
-            <div className="absolute hidden h-14 w-[600px] rotate-[-40deg] rounded-3xl bg-purple-400 opacity-30 blur-2xl filter dark:hidden lg:top-20 lg:-right-28 lg:block lg:h-10 lg:w-[600px] lg:opacity-20 lg:blur-xl xl:-right-40 xl:h-2 xl:w-[800px] xl:opacity-100" />
-            <div
-                style={{ backgroundImage: `url(${images.bannerBg})` }}
-                className="w-full py-10 lg:flex lg:h-full lg:items-center lg:py-0"
-            >
-                {/* left side */}
-                <div>
-                    <h1>
-                        The <span className="underline">Expense Tracker</span> that works for you
-                    </h1>
-                </div>
-                {/* Right side */}
-                <div>
-                    <img src={images?.bannerVector} alt="" />
-                </div>
-            </div>
-        </section>
-    );
+    return <Banner />;
 }
 
 export default Home;
