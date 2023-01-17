2e8e8548d30fdfcec60ffd0397c3720321b1df54
Raiyan <tajkierhaque@gmail.com>
2023-01-17 17:14:22 +0600

    testing server deployment [Check]

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const routes = require('../routes/root');
 
+router.get('/', (req, res) => {
+    res.send({ message: 'Success' });
+});
+
 router.use('/api/v1', routes);
 
 module.exports = router;
